ac25a08d494a3a28391ac7921a628bd48ebbbb46
jhili <jhili@myune.edu.au>
2023-08-26 14:18:22 +1000

    created generic dart file

---
+++ android/app/src/main/dart/MyGame.dart
@@ -0,0 +1,7 @@
+class Mygame extends FlameGame {
+    @override
+    Future<void> onLoad() async {...}
+
+    @override
+    void update (double dt) {...}
+    }
--- android/app/src/main/kotlin/com/example/mush/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mush/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.mush
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
 }
